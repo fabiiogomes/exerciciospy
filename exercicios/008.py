18e1992f5294f7714cd62538b196c2fc17996295
Fabio Gomes <fabiogsilva95@gmail.com>
2018-10-17 04:36:34 -0300

    conversor basico de medidas

---
+++ exercicios/008.py
@@ -0,0 +1,5 @@
+metros = float(input('Digite a quantidade de metros: '))
+cm = metros * 100
+mm = metros * 1000
+
+print('{} metros \n{:.0f} centrimentos \n{:.0f} milimetros'.format(metros, cm, mm))
